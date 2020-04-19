fffa60c95024c21646a8f0aba95b0d0be51e3155
mtriebes1 <59945830+mtriebes1@users.noreply.github.com>
2020-04-19 16:37:07 -0700

    updated array class

--- RArray.java
+++ RArray.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 
 public class RArray implements RValue {
 
+    public Token arrType = null;
     public Token arrName = null;
+    public int arrSize = null;
     public ArrayList<Expr> arrList = new ArrayList<>();
 
     public void accept(Visitor visitor) throws MyPLException {
