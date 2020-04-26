0706bb22cfd398ec0229f60a530d7acb320a5127
Alex Lloyd <60119794+alloyd2@users.noreply.github.com>
2020-04-26 13:24:53 -0700

    Add files via upload

---
+++ ArrDeclStmt.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class ArrDeclStmt implements RValue {
+
+    public Token arrName = null;
+    public Token arrType = null;
+    public Expr arrSize = null;
+    public ArrayList<Expr> arrList = new ArrayList<>();
+
+    public void accept(Visitor visitor) throws MyPLException {
+        visitor.visit(this);
+      }
+}
